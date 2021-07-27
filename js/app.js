3033a68f1121029eb8ffd61b3c35f065709cf33b
Munes Yasin <m2nesyasin@gmail.com>
2021-07-28 00:14:50 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 'use strict';
 
 
-
-let meal = prompt('Do I love mansaf?').toLowerCase()
+function qua(x){
+let meal = x
 while(meal !='yes' && meal !='y' && meal != "no" && meal!='n'){
     meal = prompt('Your answer must be yes or no or n or y ')
 }
 
-
-
- 
 switch(meal){
     case 'yes' :
     case 'y' :
@@ -24,48 +21,14 @@ switch(meal){
      
         
 }
-let nation = prompt("Am I jordanian?").toLowerCase()
-while(nation !='yes' && nation !='y' && nation != "no" && nation!='n'){
-    nation = prompt('Your answer must be yes or no or n or y ')
-}
-switch(nation){
-    case 'yes' :
-    case 'y' :
-        //console.log('yes');
-        alert('thats true')
-        break;
-    
-        
-    case 'no' : 
-    case 'n' :
-        //console.log('no');
-        alert('thats false')
-        break;    
-     
-        
-}
-
-let interest = prompt("Am I interesting in software developer ?").toLowerCase()
-while(interest !='yes' && interest !='y' && interest != "no" && interest!='n'){
-    interest = prompt('Your answer must be yes or no or n or y ')
-}
-switch(interest){
-    case 'yes' : 
-    case 'y' :
-        //console.log('yes');
-        alert('thats true')
-        break;
-   
-        
-    case 'no' : 
-    case 'n' :
-        //console.log('no');
-        alert('thats true')
-        break;    
-     
-      
 } 
-let game = prompt("Do I play Pubg ?").toLowerCase()
+qua(prompt('Do I love mansaf?').toLowerCase())
+qua(prompt('Am I jordanian?').toLowerCase())
+qua(prompt('Am I interesting in software developer ?').toLowerCase())
+
+
+function quas(x){
+let game = x
 while(game !='yes' && game !='y' && game != "no" && game!='n'){
     game = prompt('Your answer must be yes or no or n or y ')
 }
@@ -84,27 +47,9 @@ switch(game){
         break;    
      
 }
-let sport = prompt("Do I like sport ?").toLowerCase()
-while(sport !='yes' && sport !='y' && sport != "no" && sport!='n'){
-    sport = prompt('Your answer must be yes or no or n or y ')
-}
-switch(sport){
-    case 'yes' : 
-    case 'y' :
-        //console.log('yes');
-        alert('thats true ')
-        break;
-  
-       
-    case 'no' :
-    case 'n' :
-        //console.log('no');
-        alert('thats false')
-        break;    
-     
-        
-}
-
+} 
+quas(prompt("Do I play Pubg ?").toLowerCase())
+quas(prompt("Do I like sport?").toLowerCase())
 
 let username = prompt('Whats your name ?')
 
@@ -120,112 +65,36 @@ if(wel == true){
   else{
     alert('We will improve it')
   }
+
+
 let counter=0
+function quastion(x,y){
 for(let i =0 ; i<4;i++){
-  let q1=parseInt(prompt('Choose no. between 1-10'))
-  if(q1==5){
+  let q1=parseInt(prompt(x))
+  if(q1==y){
       alert('Thats great its correct ^_^')
       counter=counter+1;
       break;
   }
 
-  else if(q1<5){
+  else if(q1<y){
       alert('its lower')
+      
+  }
+  else if(q1>y){
+      alert('its higher')
+      
   }
   else{
-      alert('its higher')
+      alert('plz input no. you lost one attempt ')
   }
-} 
-for(let i =0 ; i<4;i++){
-    let q1=parseInt(prompt('Choose no. between 11-20'))
-    if(q1==17){
-        alert('Thats great its correct ^_^')
-        counter=counter+1;
-        break;
-    }
-  
-    else if(q1<17){
-        alert('its lower')
-    }
-    else{
-        alert('its higher')
-    }
-  } 
-  for(let i =0 ; i<4;i++){
-    let q1=parseInt(prompt('Choose no. between 21-30'))
-    if(q1==25){
-        alert('Thats great its correct ^_^')
-        counter=counter+1;
-        break;
-    }
-  
-    else if(q1<25){
-        alert('its lower')
-    }
-    else{
-        alert('its higher')
-    }
-  } 
-  for(let i =0 ; i<4;i++){
-    let q1=parseInt(prompt('Choose no. between 31-40'))
-    if(q1==39){
-        alert('Thats great its correct ^_^')
-        counter=counter+1;
-        break;
-    }
-  
-    else if(q1<39){
-        alert('its lower')
-    }
-    else{
-        alert('its higher')
-    }
-  } 
-  for(let i =0 ; i<4;i++){
-    let q1=parseInt(prompt('Choose no. between 41-50'))
-    if(q1==43){
-        alert('Thats great its correct ^_^')
-        counter=counter+1;
-        break;
-    }
-  
-    else if(q1<43){
-        alert('its lower')
-    }
-    else{
-        alert('its higher')
-    }
-  } 
-  for(let i =0 ; i<4;i++){
-    let q1=parseInt(prompt('Choose no. between 51-60'))
-    if(q1==54){
-        alert('Thats great its correct ^_^')
-        counter=counter+1;
-        break;
-    }
-  
-    else if(q1<54){
-        alert('its lower')
-    }
-    else{
-        alert('its higher')
-    }
-  } 
-  for(let i =0 ; i<4;i++){
-    let q1=parseInt(prompt('Choose no. between 61-70'))
-    if(q1==62){
-        alert('Thats great its correct ^_^')
-        counter=counter+1;
-        break;
-    }
-  
-    else if(q1<62){
-        alert('its lower')
-    }
-    else{
-        alert('its higher')
-    }
-  } 
+} }
+quastion('Choose no. between 1-10', 5);
+quastion('Choose no. between 11-20', 14);
+quastion('Choose no. between 21-30', 27);
+quastion('Choose no. between 31-40', 36);
+quastion('Choose no. between 41-50', 50);
+quastion('Choose no. between 51-60', 54);
   
   
 
